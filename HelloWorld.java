3ccc08da1d33a7c6531a2bc1c9909d64221b9524
nabilptra <nabilptra@upi.edu>
2025-02-12 16:33:01 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author lenovo
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Nabil 2305380 senang belajar java script!");
+    }
+}
