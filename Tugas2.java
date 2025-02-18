dc6a015825de0a7ee1742d7541ca89fbb69195d1
nabilptra <nabilptra@upi.edu>
2025-02-18 14:45:12 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package nabil2305380.tugas2;
+
+//import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Tugas2 {
+
+    public static void main(String[] args) {
+       // Scanner input = new Scanner(System.in);
+        
+        //Soal Nomor 1
+       /* double a = 2.2;
+        double b = 3.3;
+        double c = 4.4;
+
+        System.out.println("Hasil Penjumlahan: " + (a + b + c));
+        System.out.println("Hasil Pengurangan: " + (a - b - c));
+        System.out.println("Hasil Perkalian: " + (a * b * c));
+        System.out.println("Hasil Pembagian: " + (a / b / c));
+       
+       
+       //Soal Nomor 2
+        System.out.print("Masukkan ranking: ");
+       int ranking = input.nextInt();
+       
+       System.out.print("Naik Kelas?(true/false): ");
+       boolean naikKelas = input.nextBoolean();
+       
+       String hasil = (ranking == 1 && naikKelas) ? "Selamat! Anda mendapatkan hadiah!" 
+                                                  :"Maaf, Anda tidak mendapatkan hadiah.";
+       
+       System.out.println(hasil);*/
+       
+       //Soal Nomor 3
+       char charA = 'A';
+       char charB ='B';
+       
+       int sum = charA + charB;
+       
+       System.out.println("Kode ASCII dari 'A': " + (int) charA);
+       System.out.println("Kode ASCII dari 'B': " + (int) charB);
+       System.out.println("Hasil penjumlahan ASCII 'A' + 'B': " + sum);
+        
+       
+       
+    
+    }
+}
